649c20129196de896efb65656b9ddb6ede4b4360
Aman Singh <aman76077singh@gmail.com>
2026-08-26 00:41:34 +0530

    Add files via upload
    Added 01 web development portfolio files

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+
+    if (form) {
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            alert("Thank you! Your message has been submitted.");
+        });
+    }
+});
